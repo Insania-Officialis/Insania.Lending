acdbe3c2268e515c4878475295cba43b092d98d7
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-10-03 13:57:31 +0300

    - Корректировка фона - Рефакторинг размеров - Рефакторинг шрифтов - Вёрстка компонента о расах

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@
 
 import { Menu } from '../components/menu/menu.jsx';
 import { AboutTheProject } from '../components/about_the_project/about_the_project.jsx';
+import { AboutTheRaces } from '../components/about_the_races/about_the_races.jsx';
 
 export default function Index() {
     //Формирование клиента запросов
@@ -11,8 +12,11 @@ export default function Index() {
     return (
         <QueryClientProvider client={queryClient}>
             <div className='index__block'>
-                <Menu />
-                <AboutTheProject />
+                <div className='index__block-inner'>
+                    <Menu />
+                    <AboutTheProject />
+                    <AboutTheRaces />
+                </div>
             </div>
         </QueryClientProvider>
     )
