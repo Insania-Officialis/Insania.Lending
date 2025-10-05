2130792a03f61ba68c597a1726e35e768a2dc905
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-10-05 08:57:43 +0300

    - Вёрстка блока информации о расе

--- src/components/about_the_races/about_the_races.jsx
+++ src/components/about_the_races/about_the_races.jsx
@@ -3,79 +3,96 @@ export function AboutTheRaces({ races, nations }) {
     //Моковый массив рас
     const mockRaces = [
         {
-            "id": 1,
-            "name": "Ихтид",
-            "description": "Ихтиды - "
+            'id': 1,
+            'name': 'Ихтид',
+            'active': false,
+            'description': 'Ихтиды - ',
+            countries: ['Царство Банду']
         },
         {
-            "id": 4,
-            "name": "Мраат",
-            "description": "Мрааты - "
+            'id': 4,
+            'name': 'Мраат',
+            'active': false,
+            'description': 'Мрааты - '
         },
         {
-            "id": 5,
-            "name": "Человек",
-            "description": "Люди - "
+            'id': 5,
+            'name': 'Человек',
+            'active': false,
+            'description': 'Люди - '
         },
         {
-            "id": 6,
-            "name": "Вампир",
-            "description": "Вампиры - "
+            'id': 6,
+            'name': 'Вампир',
+            'active': false,
+            'description': 'Вампиры - '
         },
         {
-            "id": 7,
-            "name": "Эльф",
-            "description": "Эльфы - "
+            'id': 7,
+            'name': 'Эльф',
+            'active': false,
+            'description': 'Эльфы - '
         },
         {
-            "id": 8,
-            "name": "Метаморф",
-            "description": "Метаморфы - "
+            'id': 8,
+            'name': 'Метаморф',
+            'active': false,
+            'description': 'Метаморфы - '
         },
         {
-            "id": 9,
-            "name": "Орк",
-            "description": "Орки - "
+            'id': 9,
+            'name': 'Орк',
+            'active': false,
+            'description': 'Орки - '
         },
         {
-            "id": 10,
-            "name": "Дварф",
-            "description": "Дварфы - "
+            'id': 10,
+            'name': 'Дварф',
+            'active': true,
+            'description': 'Дварфы - ',
+            countries: ['Королевство Нордер', 'Королевство Вервирунг']
         },
         {
-            "id": 11,
-            "name": "Тролль",
-            "description": "Тролли - "
+            'id': 11,
+            'name': 'Тролль',
+            'active': false,
+            'description': 'Тролли - '
         },
         {
-            "id": 12,
-            "name": "Гоблин",
-            "description": "Гоблины - "
+            'id': 12,
+            'name': 'Гоблин',
+            'active': false,
+            'description': 'Гоблины - '
         },
         {
-            "id": 13,
-            "name": "Огр",
-            "description": "Огры - "
+            'id': 13,
+            'name': 'Огр',
+            'active': false,
+            'description': 'Огры - '
         },
         {
-            "id": 14,
-            "name": "Альв",
-            "description": "Альвы - "
+            'id': 14,
+            'name': 'Альв',
+            'active': false,
+            'description': 'Альвы - '
         },
         {
-            "id": 16,
-            "name": "Элвин",
-            "description": "Элвины - "
+            'id': 16,
+            'name': 'Элвин',
+            'active': false,
+            'description': 'Элвины - '
         },
         {
-            "id": 17,
-            "name": "Дану",
-            "description": "Дану - "
+            'id': 17,
+            'name': 'Дану',
+            'active': false,
+            'description': 'Дану - '
         },
         {
-            "id": 15,
-            "name": "Антропозавр",
-            "description": "Антропозавры - "
+            'id': 15,
+            'name': 'Антропозавр',
+            'active': false,
+            'description': 'Антропозавры - '
         },
     ];
 
@@ -91,7 +108,7 @@ export function AboutTheRaces({ races, nations }) {
                 <div className='about-the-races__block-races-list'>
                     {mockRaces.map(race => {
                         return (
-                            <div key={race.id} className={'about-the-races__block-races-list-item' + (race.id === 1 ? ' about-the-races__block-image-race--active' : '')} >
+                            <div key={race.id} className={'about-the-races__block-races-list-item' + (race.active ? ' about-the-races__block-image-race--active' : '')} >
                                 <img src='favicon.ico' alt='' className='about-the-races__image-race' />
                                 <span>{race.name}</span>
                             </div>
@@ -99,6 +116,25 @@ export function AboutTheRaces({ races, nations }) {
                     })}
                 </div>
                 <div className='about-the-races__block-race'>
+                    <div className='about-the-races__block-image-race'>
+                        <img src='favicon.ico' alt='' className='about-the-races__image-race' />
+                    </div>
+                    <span className='about-the-races__text-title'>{mockRaces.find(x => x.active)?.name}</span>
+                    <svg width='278' height='5' viewBox='0 0 278 5' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M277.753 2.5H3.75342M3.75342 2.5L2.25342 1L0.753418 2.5L2.25342 4L3.75342 2.5Z' stroke='url(#paint0_linear_29_320)' />
+                        <defs>
+                            <linearGradient id='paint0_linear_29_320' x1='277.753' y1='2.5' x2='-3.24659' y2='2.5' gradientUnits='userSpaceOnUse'>
+                                <stop stopColor='white' stopOpacity='0' />
+                                <stop offset='1' stopColor='white' />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                    <span>{mockRaces.find(x => x.active)?.description}</span>
+                    <span>{mockRaces.find(x => x.active)?.countries?.join(', ')}</span>
+                    <svg height='6' viewBox='0 0 515 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M0.866667 3L3.75342 5.88675L6.64017 3L3.75342 0.113249L0.866667 3ZM514.64 3.00004L511.753 0.113293L508.867 3.00004L511.753 5.8868L514.64 3.00004ZM3.75342 3L3.75342 3.5L511.753 3.50004L511.753 3.00004L511.753 2.50004L3.75342 2.5L3.75342 3Z' fill='#C0C0C0' fillOpacity='0.4' />
+                    </svg>
+
                 </div>
             </div>
         </div>
